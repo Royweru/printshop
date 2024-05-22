c3042e03d1ff13c31b7b96c4fdc019954f0cccee
Royweru <royezi17@gmail.com>
2024-05-22 16:56:37 +0300

    new file:   src/assets/logo.jpg 	modified:   src/components/footer/index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,12 +5,18 @@ const Footer = () => {
   return (
     <div className=" w-full flex flex-col 
      text-4xl font-poetsen  gap-y-3
-    font-semibold py-5">
+    font-semibold  py-8">
     <div className=" w-full ">
      <MarqueeText txt1="ON SALE! BUY NOW BUY NOW ON SALE ON SALE BUY NOW"/>
     </div>
-     <div className=" container md:px-8 ">
-           FOOTER
+     <div className=" 
+     container 
+     md:px-8 
+     grid grid-cols-2 md:grid-cols-5 
+     md:gap-2 gap-4">
+          <div className=" md:col-span-2 md:px-12 ">
+            <img src=".../assets" alt="" />
+          </div>
      </div>
       </div>
   )
