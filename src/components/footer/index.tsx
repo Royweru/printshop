ac59550ad9dcbaaeac4f2c4ea4ae23d02fc2d934
Royweru <royezi17@gmail.com>
2024-05-22 16:16:35 +0300

    modified:   src/components/footer/index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,11 @@
 
 const Footer = () => {
   return (
-    <div className=" container  md:px-8 text-4xl font-poetsen font-semibold">Footer</div>
+    <div className=" container  md:px-8 
+    text-4xl font-poetsen 
+    font-semibold py-5">
+      Footer
+      </div>
   )
 }
 
