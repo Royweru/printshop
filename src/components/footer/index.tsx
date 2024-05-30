099f8767c30e8dc6996e49f98fae200fc926c7dc
Royweru <royezi17@gmail.com>
2024-05-30 11:48:12 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/footer/index.tsx 	modified:   src/components/tied-gallery.tsx 	new file:   src/data/constants.tsx 	modified:   src/main.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,25 +1,65 @@
-import { MarqueeText } from "./marquee"
-
+import { footerDetails } from "../../data/constants";
+import { MarqueeText } from "./marquee";
 
 const Footer = () => {
   return (
-    <div className=" w-full flex flex-col 
-     text-4xl font-poetsen  gap-y-3
-    font-semibold  py-8">
-    <div className=" w-full ">
-     <MarqueeText txt1="ON SALE! BUY NOW BUY NOW ON SALE ON SALE BUY NOW"/>
-    </div>
-     <div className=" 
+    <div
+      className=" w-full 
+      flex flex-col 
+     text-4xl 
+     font-poetsen  
+     gap-y-3
+    font-semibold  
+     py-12
+     bg-n-3 
+    rounded-t-md
+     border-t-[1.5px]
+      border-n-1 backdrop-blur-sm"
+    >
+      <div className=" w-full ">
+        <MarqueeText txt1="ON SALE! BUY NOW BUY NOW ON SALE ON SALE BUY NOW" />
+      </div>
+      <div
+        className=" 
      container 
      md:px-8 
-     grid grid-cols-2 md:grid-cols-5 
-     md:gap-2 gap-4">
-          <div className=" md:col-span-2 md:px-12 ">
-           <img src=".../assets" alt="" />
-          </div>
-     </div>
+     grid 
+     grid-cols-2
+      md:grid-cols-5 
+      lg:grid-cols-7 
+     md:gap-2 gap-2"
+      >
+        <div className=" md:col-span-2  md:px-7 ">
+          <img src="..../assets/logo.jpg" alt="" />
+        </div>
+        <div className=" md:col-span-2 lg:col-span-3">
+          <ul
+            className=" w-full  
+            flex 
+             flex-row 
+             gap-x-4 
+             justify-center 
+             items-center
+              font-nunito
+              "
+          >
+            {footerDetails.map(({ id, label, path }) => (
+              <a href={path} key={id}>
+                <li className=" text-sm text-color-2 hover:text-n-2">
+                  {label}
+                </li>
+              </a>
+            ))}
+          </ul>
+        </div>
+        <div className=" col-span-2 px-2 flex justify-center items-center">
+          <p className=" text-xs font-light font-nunito text-n-4">
+             2024 all rights maintained
+          </p>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ src/data/constants.tsx
@@ -0,0 +1,23 @@
+export const footerDetails=[
+    {
+        id:1,
+        label:"Services",
+        path:"#services"
+    },
+    {
+        id:2,
+        label:"Products",
+        path:"#services"
+    },
+    {
+        id:3,
+        label:"About",
+        path:"#services"
+    },
+    {
+        id:4,
+        label:"Services",
+        path:"#services"
+    },
+
+]
