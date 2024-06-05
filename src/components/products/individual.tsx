42ef4b045a26cfefc7b33e31e5c8896d7d8e7510
Royweru <royezi17@gmail.com>
2024-06-05 20:37:29 +0300

    modified:   src/components/products/gallery-tab.tsx 	modified:   src/components/products/individual.tsx

--- src/components/products/individual.tsx
+++ src/components/products/individual.tsx
@@ -5,53 +5,6 @@ import ImageCarousel from "./image-carousel";
 import { Tabs } from "../ui/tabs";
 
 export const IndividualProduct = () => {
-  const tabs = [
-    {
-      title: "Product",
-      value: "product",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Services",
-      value: "services",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Playground",
-      value: "playground",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Content",
-      value: "content",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Random",
-      value: "random",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <DummyContent />
-        </div>
-      ),
-    },
-  ];
   const { slug } = useParams();
   const product = productsDetails.find((item) => item.slug === slug);
   return (
@@ -60,19 +13,8 @@ export const IndividualProduct = () => {
        rounded-xl lg:py-2 py-2 grid md:grid-cols-2 lg:grid-cols-3 md:gap-4 lg:gap-1"
     >
       <div className=" w-full col-span-1 md:col-span-1 lg:col-span-2 ">
-        <Tabs tabs={tabs} />
+        <ProductGallery images={product?.images} />
       </div>
     </div>
-  );
-};
-const DummyContent = () => {
-  return (
-    <img
-      src="https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-      alt="dummy image"
-      width="1000"
-      height="1000"
-      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
-    />
   );
 };
